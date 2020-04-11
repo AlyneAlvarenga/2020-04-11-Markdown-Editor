cb9f41c2ff067e83782d790c2d3f44758b2d3720
AlyneAlvarenga <alynevs@gmail.com>
2020-04-11 14:54:45 -0400

    Updates

---
+++ precache-manifest.9e98168ddd6ffa664a5f1892234cf658.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f600ec1f8447fa6278d5e50e1ac3f6",
+    "url": "/2020-04-11-Markdown-Editor/index.html"
+  },
+  {
+    "revision": "e450eec1451a4700d317",
+    "url": "/2020-04-11-Markdown-Editor/static/css/2.56a1fa8e.chunk.css"
+  },
+  {
+    "revision": "5cc23c65979a437619e6",
+    "url": "/2020-04-11-Markdown-Editor/static/css/main.d0d485c7.chunk.css"
+  },
+  {
+    "revision": "e450eec1451a4700d317",
+    "url": "/2020-04-11-Markdown-Editor/static/js/2.ad0d3166.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/2020-04-11-Markdown-Editor/static/js/2.ad0d3166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cc23c65979a437619e6",
+    "url": "/2020-04-11-Markdown-Editor/static/js/main.8a3148b2.chunk.js"
+  },
+  {
+    "revision": "928d657d827aa9e4e3a2",
+    "url": "/2020-04-11-Markdown-Editor/static/js/runtime-main.e84c225b.js"
+  }
+]);
