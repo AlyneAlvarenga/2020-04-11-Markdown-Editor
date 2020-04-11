873a0aa88a655614212059f4bb1900506a487c48
AlyneAlvarenga <alynevs@gmail.com>
2020-04-11 14:28:36 -0400

    set up editor and output, started styling

--- markdown-editor/src/App.js
+++ markdown-editor/src/App.js
@@ -1,19 +1,70 @@
 import React, { Component } from 'react';
+import CodeMirror from 'react-codemirror';
+import 'codemirror/lib/codemirror.css';
+import marked from 'marked';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-
+      code: "## Code",
     }
   }
 
-  render() {
-    return (
-      <div className="App">
+  updateCode = (newCode) => {
+    this.setState({
+      code: newCode,
+    });
+  }
 
-      </div>
+  rawMarkup = () => {
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: true,
+      smartLists: true,
+      smartypants: false,
+      // highlight: function (code) {
+      //   return hljs.highlightAuto(code).value
+      // }
+    })
+
+    const rawMarkup = marked(this.state.code, {sanitize: true});
+
+    return {
+      __html: rawMarkup
+    }
+  }
+
+
+  render() {
+    const options = {
+      lineNumbers: true,
+      mode: 'markdown',
+    }
+
+    return (
+      <main>
+        <div className="wrapper">
+          <h1 className="alignCenter">Welcome to the Markdown Editor</h1>
+          <p className="alignCenter">Start typing your markdown text on the editor. See the output in HTML on the right.</p>
+  
+          <section>
+            <div className="editorContainer">
+              <h2 className="sectionTitles">Editor</h2>
+              <CodeMirror value={this.state.code} onChange={this.updateCode} options={options}/>
+            </div>
+            <div className="outputContainer">
+              <h2 className="sectionTitles">Output</h2>
+              <div id="outputText" dangerouslySetInnerHTML={this.rawMarkup()}></div>
+            </div>
+          </section>
+        </div>
+      </main>
     )
   }
 }
